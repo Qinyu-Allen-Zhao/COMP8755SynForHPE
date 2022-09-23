962925bf25cb3b8f3664ad7780576cf4cb35b4a8
qinyu.zhao <u7212335@anu.edu.au>
2022-09-23 16:15:46 +1000

    Learn the residual.

--- PoseGAN/generator.py
+++ PoseGAN/generator.py
@@ -8,26 +8,27 @@ class Generator(nn.Module):
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.Conv2d(nz, ngf * 8, 3, 1, 1, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
-            # state size. (ngf*8) x 4 x 4
-            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            # state size. (ngf*8) x 64 x 64
+            nn.Conv2d(ngf * 8, ngf * 4, 3, 1, 1, bias=False),
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
-            # state size. (ngf*4) x 8 x 8
-            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            # state size. (ngf*4) x 64 x 64
+            nn.Conv2d(ngf * 4, ngf * 2, 3, 1, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
-            # state size. (ngf*2) x 16 x 16
-            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
+            # state size. (ngf*2) x 64 x 64
+            nn.Conv2d(ngf * 2, ngf, 3, 1, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
-            # state size. (ngf) x 32 x 32
-            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
+            # state size. (ngf) x 64 x 64
+            nn.Conv2d(ngf, nc, 3, 1, 1, bias=False),
             nn.Tanh()
             # state size. (nc) x 64 x 64
         )
 
     def forward(self, inp):
-        return self.main(inp)
+        res = self.main(inp)
+        return inp + res
