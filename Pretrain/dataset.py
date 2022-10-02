33d2f738b25145a7b1d348e28d9e096161825abe
Qinyu Zhao <qinyu.zhao@anu.edu.au>
2022-10-02 18:27:52 +1100

    Read images and smpls to train

--- Pretrain/dataset.py
+++ Pretrain/dataset.py
@@ -1,18 +1,30 @@
+import os
+import joblib
 import numpy as np
 from torch.utils.data import Dataset
 
 
 class ImageDataset(Dataset):
-    def __init__(self, img_npz_file, ann_npz_file, transform=None, target_transform=None):
+    def __init__(self, img_path, ann_path, transform=None, target_transform=None):
         # Read data files
-        self.images = np.load(img_npz_file)['arr_0']
-        self.labels = np.load(ann_npz_file)['arr_0'].astype(np.long)
+        self.images = []
+        self.labels = []
+        for sub_id in range(103):
+            for img_id in range(500):
+                image_file = os.path.join(img_path, "sub{}_{:0>8}.png".format(sub_id, img_id))
+                self.images.append(image_file)
+            smpls = joblib.load('sub%d.pkl' % sub_id)['smpls']
+            self.labels.append(smpls)
+        self.labels = np.concatenate(self.labels, axis=0)
+        print("Read images: ", len(self.images))
+        print("Read poses: ", self.labels.shape)
+
         # self.labels = np.eye(10)[self.labels]  # I found there was no need
         self.transform = transform
         self.target_transform = target_transform
 
     def __len__(self):
-        return len(self.labels)
+        return len(self.images)
 
     def __getitem__(self, index):
         # Read the specific image and its label
--- Pretrain/train.py
+++ Pretrain/train.py
@@ -16,12 +16,9 @@ batch_size = 64
 num_epochs = 140
 
 # Dataset
-train_img_file = './data/kmnist-npz/kmnist-train-imgs.npz'
-train_ann_file = './data/kmnist-npz/kmnist-train-labels.npz'
-val_img_file = './data/kmnist-npz/kmnist-val-imgs.npz'
-val_ann_file = './data/kmnist-npz/kmnist-val-labels.npz'
-test_img_file = './data/kmnist-npz/kmnist-test-imgs.npz'
-test_ann_file = './data/kmnist-npz/kmnist-test-labels.npz'
+img_path = '/content/drive/MyDrive/datasets/synthesis_dataset/'
+ann_path = '/content/drive/MyDrive/datasets/syn_poses/'
+
 # Transform
 train_transform = transforms.Compose([
     # Normalize to [-1, 1]
@@ -30,36 +27,24 @@ train_transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.ConvertImageDtype(torch.float),
     transforms.Normalize((0.5,), (0.5,)),
+    transforms.Resize((256, 256)),
     # Data augmentation
     transforms.RandomHorizontalFlip(0.5),
     transforms.Pad(padding=4, fill=0),
-    transforms.RandomCrop(size=(28, 28))
+    transforms.RandomCrop(size=(256, 256))
 ])
 
-val_transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.ConvertImageDtype(torch.float),
-    transforms.Normalize((0.5,), (0.5,))
-])
 # Dataset instances
-train_set = ImageDataset(train_img_file, train_ann_file,
+train_set = ImageDataset(img_path, ann_path,
                          transform=train_transform, target_transform=None)
-val_set = ImageDataset(val_img_file, val_ann_file,
-                       transform=val_transform, target_transform=None)
-test_set = ImageDataset(test_img_file, test_ann_file,
-                        transform=val_transform, target_transform=None)
 # Data loader
 train_loader = torch.utils.data.DataLoader(train_set,
                                            batch_size=batch_size, shuffle=True)
-val_loader = torch.utils.data.DataLoader(val_set,
-                                         batch_size=batch_size, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_set,
-                                          batch_size=batch_size, shuffle=True)
 
 # Model, loss, optimizer, and writer for Tensorboard
 gpu_available = torch.cuda.is_available()
 model = get_pose_net(cfg, is_train=True)
-loss_func = nn.CrossEntropyLoss()
+loss_func = nn.MSELoss()
 if gpu_available:
     model.cuda()
     loss_func.cuda()
@@ -91,37 +76,15 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
 
-    PATH = './checkpoints/kmnist_net_{:02d}.pth'.format(epoch)
+    PATH = './checkpoints/pretrain_{:02d}.pth'.format(epoch)
     torch.save(model.state_dict(), PATH)
 
     train_acc, train_loss = eva_acc_and_loss(train_loader, model, gpu_available)
     writer.add_scalar("Accuracy/train", train_acc, epoch)
     writer.add_scalar("Loss/train", train_loss, epoch)
 
-    val_acc, val_loss = eva_acc_and_loss(val_loader, model, gpu_available)
-    writer.add_scalar("Accuracy/val", val_acc, epoch)
-    writer.add_scalar("Loss/val", val_loss, epoch)
-
-    if val_loss < best_loss:
-        # Record the best model based on the val performance
-        best_model_epoch = epoch
-        best_loss = val_loss
-        torch.save(model.state_dict(), './best_model.pth')
-
     # Print the model performance
     curr = time.time()
     print('Epoch: {}, Time: {}'.format(epoch, round(curr - end, 3)))
     print('---- train accuracy: {:.4}%, train loss: {:.4}'.format(train_acc, train_loss))
-    print('---- validation accuracy: {:.4}%, validation loss: {:.4}'.format(val_acc, val_loss))
     end = curr
-
-# Load and evaluate the best model
-PATH = './best_model.pth'
-best_model = get_pose_net(cfg, is_train=False, model_path=PATH)
-if gpu_available:
-    best_model.cuda()
-test_acc, test_loss = eva_acc_and_loss(test_loader, best_model, gpu_available)
-print()
-print('Best Model')
-print('---- epoch: {}'.format(best_model_epoch))
-print('---- test accuracy: {:.4}%, test loss: {:.4}'.format(test_acc, test_loss))
